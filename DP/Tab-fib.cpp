0b5b06e92dd1412a77cc21c33c85f2758e35fd36
Addi-11 <saddi.5131@gmail.com>
2019-10-02 15:33:55 +0530

    tabulation form fibonacci

---
+++ DP/Tab-fib.cpp
@@ -0,0 +1,16 @@
+//Tabulation--Bottom Up approach all values are calculated from 0 to n
+#include<bits/stdc++.h>
+
+int fib( int n){
+    int f[n+1];
+    f[0] = 0; f[1] = 1;
+    for(int i = 2; i <= n; i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+    return f[n];
+}
+
+int main(){
+    std::cout << fib(8);
+    return 0;
+}
